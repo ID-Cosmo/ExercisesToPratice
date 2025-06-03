7fe2bb1f7a71a8699fa53fe7ef2e98127ed17398
Tiago Borges <tiagoluizborges0@gmail.com>
2025-06-02 22:25:02 -0300

    Exercicio para banco

---
+++ banco.py
@@ -0,0 +1,37 @@
+class ContaBancaria:
+    def __init__(self, saldo_bancario):
+        self.__saldo_bancario = saldo_bancario
+    def depositar(self,valor_deposito):
+        self.__saldo_bancario += valor_deposito
+    def sacar(self,valor_sacar):
+        self.__saldo_bancario -= valor_sacar
+
+    def mostrar_saldo(self):
+        return f"Saldo atual: {self.__saldo_bancario}"
+    
+ContaBancaria1 = ContaBancaria(1500)
+
+# ContaBancaria1.depositar(100)
+# ContaBancaria1.sacar(200)
+# print(ContaBancaria1.mostrar_saldo())
+
+opcao = input("Selecione uma das opções: \n1 - Mostrar saldo  \n2 - Sacar  \n3 - Depositar\n")
+
+if opcao == "1":
+    print(ContaBancaria1.mostrar_saldo())
+
+elif opcao == "2":
+    i = int(input("Quanto voce quer sacar:"))
+    ContaBancaria1.sacar(i)
+    print (f"Voce sacou R${i}")
+    print (ContaBancaria1.mostrar_saldo())
+
+elif opcao == "3":
+    i = int(input("Quanto voce quer depositar:"))
+    ContaBancaria1.depositar(i)
+    print (f"Voce depositou R${i}")
+    print (ContaBancaria1.mostrar_saldo())
+    
+else:
+    print("Opcao invalida")
+    
